b3d56c3bc97e26c6cc874b26d3d5ff5322268866
Wipark93 <wipark93@gmail.com>
2017-05-17 23:25:13 -0700

    added
    sdf

--- BeefBot.py
+++ BeefBot.py
@@ -1,21 +1,21 @@
 import discord
 from discord.ext.commands import Bot
 
-import Secret
+import secret
 
 beefbot = Bot(command_prefix="!")
 
 @beefbot.event
 async def on_read():
-    print("Client logged in")
+	print("Client logged in")
 
 @beefbot.command()
 async def hello(args):
-    return await beefbot.say("Hello World!")
+	return await beefbot.say("Hello World!")
 
 @beefbot.command()
 async def beef(args):
-    return await beefbot.say("I am winner!")
+	return await beefbot.say("I am winner!")
 
 
-beefbot.run(Secret.BOT_TOKEN)
+beefbot.run(secret.BOT_TOKEN)
