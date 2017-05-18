4c4fa9e1ad0e7e8cd8ccd45fe2ffc5dcedbc8db7
BPhizz <brandon.n.pham@gmail.com>
2017-05-18 01:16:04 -0500

    Test
    skeleton code

---
+++ BeefBot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext.commands import Bot
+
+import Secret
+
+BeefBot = Bot(command_prefix="!")
+
+@BeefBot.event
+async def on_read():
+    print("Client logged in")
+
+@BeefBot.command()
+async def hello(args):
+    return await BeefBot.say("Hello World!")
+
+@BeefBot.command()
+async def beef(args):
+    return await BeefBot.say("I am winner!")
+
+
+BeefBot.run(secrets.BOT_TOKEN)
