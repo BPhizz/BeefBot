e0ab77aeec3addefc3d7f8a024f4e4f93e82f479
Wipark93 <wipark93@gmail.com>
2017-05-17 23:49:54 -0700

    asd
    asd

--- BeefBot.py
+++ BeefBot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext.commands import Bot
 
 import secret
+import beefy
 
 beef_bot = Bot(command_prefix="!")
 
@@ -16,15 +17,11 @@ async def hello(*args):
 @beef_bot.command()
 async def beef(*args):
 	return await beef_bot.say("I am winner!")
-
-<<<<<<< HEAD
+	
+@beef_bot.command()
 async def fuDarcy(*args):
 	return await beef_bot.say("Go back to bed bitch")	
-=======
-@beef_bot.command()
-async def darcy(*args):
-	return await beef_bot.say("Whos that? :Kappa: ")
+	
 
->>>>>>> 07910a5ef64107742337e924a46856bea67f707b
 
 beef_bot.run(secret.BOT_TOKEN)
---
+++ beefy.py
@@ -0,0 +1,3 @@
+@beef_bot.command()
+async def darcy(*args):
+	return await beef_bot.say("Whos that? :Kappa: ")
