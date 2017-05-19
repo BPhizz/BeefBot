555feb50bae45cf7f2e7ff9b3c013463e8cdce90
BPhizz <brandon.n.pham@gmail.com>
2017-05-18 21:19:02 -0500

    Testing Random
    command for kyle

--- BeefBot.py
+++ BeefBot.py
@@ -1,4 +1,5 @@
 import discord
+from random import randint
 from discord.ext.commands import Bot
 
 import secret
@@ -29,6 +30,18 @@ async def KKCrispus(*args):
 @beef_bot.command()
 async def darcy(*args):
 	return await beef_bot.say("Whos that? <:Kappa:240828655385837568> ")
+
+@beef_bot.command()
+async def Kyle(*args):
+	temp = randint(1,4)
+	if temp==1
+		return await beef_bot.say("Take your shirt off!")
+	elif temp==2
+		return await beef_bot.say("Doube What?! Triple What?! Don't make me go killtacular on your ass!")
+	elif temp==3
+		return await beef_bot.say("Hey Chris, whats up?")
+	elif temp==4
+		return await beef_bot.say("[placeholder]")
 
 
 
